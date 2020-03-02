efcfded96a72b7e72c5bfe7e7b91bddc88492bae
Nathan <naykel@iinet.net.au>
2020-03-03 09:36:22 +1000

    added labs

---
+++ lab01_js/main2.js
@@ -0,0 +1,6 @@
+var num1 = 1;
+var num2 = 2;
+var num3 = 3;
+var total = num1 + num2 + num3;
+var output = "Sum of numbers is:" + total;
+console.log(output);
---
+++ lab01_js/main2.ts
@@ -0,0 +1,9 @@
+let num1: number = 1;
+let num2: number = 2;
+let num3: number = 3;
+
+let total: number =  num1 + num2 + num3;
+let output: string = "Sum of numbers is:" + total
+
+
+console.log(output)
