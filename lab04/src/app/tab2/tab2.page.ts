23bc3f686bad00923765cbacfda99e3f9078fdc8
Nathan <naykel@iinet.net.au>
2020-03-16 11:47:17 +1000

    added lab04

---
+++ lab04/src/app/tab2/tab2.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-tab2',
+    templateUrl: 'tab2.page.html',
+    styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+    loginCount: number = 0;
+
+    constructor(private router: Router) {
+
+    }
+
+    incrementCounter() {
+        this.loginCount++;
+    }
+
+    login() {
+        this.incrementCounter();
+        this.router.navigateByUrl('/account');
+    }
+}
