4d2d83ba21be6a74169db4cde442f06bf6662502
Nathan <naykel@iinet.net.au>
2020-03-17 10:18:06 +1000

    finished lab04

--- lab04/src/app/account/account.page.ts
+++ lab04/src/app/account/account.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.page.html',
-  styleUrls: ['./account.page.scss'],
+    selector: "app-account",
+    templateUrl: "./account.page.html",
+    styleUrls: ["./account.page.scss"]
 })
 export class AccountPage implements OnInit {
+    username: string;
+    constructor(private route: ActivatedRoute) {
+        console.log(ActivatedRoute);
 
-  constructor() { }
+    }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.username = this.route.snapshot.paramMap.get("username");
+    }
 }
--- lab04/src/app/tab2/tab2.page.ts
+++ lab04/src/app/tab2/tab2.page.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-    selector: 'app-tab2',
-    templateUrl: 'tab2.page.html',
-    styleUrls: ['tab2.page.scss']
+    selector: "app-tab2",
+    templateUrl: "tab2.page.html",
+    styleUrls: ["tab2.page.scss"]
 })
 export class Tab2Page {
-
     loginCount: number = 0;
+    username: string = "";
 
     constructor(private router: Router) {
-
+        console.log(this.username);
     }
 
     incrementCounter() {
@@ -20,6 +20,7 @@ export class Tab2Page {
 
     login() {
         this.incrementCounter();
-        this.router.navigateByUrl('/account');
+        // this.router.navigateByUrl('/account');
+        this.router.navigateByUrl("/account/" + this.username);
     }
 }
