ea8ea160ecc5b996dfd8a1ee39ab9827babc1d26
Nathan <naykel@iinet.net.au>
2020-03-30 15:25:57 +1000

    WIP

---
+++ lab06/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
+  },  {
+    path: 'modal',
+    loadChildren: () => import('./modal/modal.module').then( m => m.ModalPageModule)
+  },
+
+
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ lab06/src/app/tab1/tab1.page.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+    contacts = [{
+        firstName: 'Billy',
+        lastName: 'Jones',
+        email: 'billy@email.com.au'
+    },
+    {
+        firstName: 'Sue',
+        lastName: 'Williams',
+        email: 'sue@email.com.au'
+    },
+    {
+        firstName: 'Mike',
+        lastName: 'McDingle',
+        email: 'mike@email.com.au'
+    }];
+
+    constructor(private modalController:ModalController){ }
+
+    addContact(){
+        alert('hey');
+    }
+
+}
