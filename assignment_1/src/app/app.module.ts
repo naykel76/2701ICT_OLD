1942da33099960a3b776d8f1f209c3afb8dfd151
Nathan <naykel@iinet.net.au>
2020-04-05 04:41:12 +1000

    added form-modal

--- assignment_1/src/app/app.module.ts
+++ assignment_1/src/app/app.module.ts
@@ -9,10 +9,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { FormModalPageModule } from './modals/form-modal/form-modal.module';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormModalPageModule],
   providers: [
     StatusBar,
     SplashScreen,
