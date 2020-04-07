a55d304b9c68066f273c444756d835fd9ce3d961
Nathan <naykel@iinet.net.au>
2020-04-08 06:53:08 +1000

    added lab07

--- assignment_1/src/app/app-routing.module.ts
+++ assignment_1/src/app/app-routing.module.ts
@@ -2,8 +2,29 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+
+  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule) },
+//   { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
+//   {
+//     path: 'forms',
+//     loadChildren: () => import('./forms/forms.module').then( m => m.FormsPageModule)
+//   },
+//   {
+//     path: 'reports',
+//     loadChildren: () => import('./reports/reports.module').then( m => m.ReportsPageModule)
+//   },
+//   {
+//     path: 'forms',
+//     loadChildren: () => import('./pages/forms/forms.module').then( m => m.FormsPageModule)
+//   },
+//   {
+//     path: 'reports',
+//     loadChildren: () => import('./pages/reports/reports.module').then( m => m.ReportsPageModule)
+//   },
+//   {
+//     path: 'home',
+//     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+//   },
 ];
 
 @NgModule({
